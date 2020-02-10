176de96acc08261c8483f9cb7e3c102081684970
Hisham Muhammad <hisham@gobolinux.org>
2020-02-10 12:38:15 +0100

    regression test for #53

--- spec/statement/forin_spec.lua
+++ spec/statement/forin_spec.lua
@@ -1,27 +1,55 @@
 local tl = require("tl")
 
 describe("forin", function()
-   it("with a single variable", function()
-      local tokens = tl.lex([[
-         local t = { 1, 2, 3 }
-         for i in ipairs(t) do
-            print(i)
-         end
-      ]])
-      local _, ast = tl.parse_program(tokens)
-      local errors = tl.type_check(ast)
-      assert.same({}, errors)
-   end)
-   it("with two variables", function()
-      local tokens = tl.lex([[
-         local t = { 1, 2, 3 }
-         for i, v in ipairs(t) do
-            print(i, v)
-         end
-      ]])
-      local _, ast = tl.parse_program(tokens)
-      local errors = tl.type_check(ast)
-      assert.same({}, errors)
+   describe("ipairs", function()
+      it("with a single variable", function()
+         local tokens = tl.lex([[
+            local t = { 1, 2, 3 }
+            for i in ipairs(t) do
+               print(i)
+            end
+         ]])
+         local _, ast = tl.parse_program(tokens)
+         local errors = tl.type_check(ast)
+         assert.same({}, errors)
+      end)
+      it("with two variables", function()
+         local tokens = tl.lex([[
+            local t = { 1, 2, 3 }
+            for i, v in ipairs(t) do
+               print(i, v)
+            end
+         ]])
+         local _, ast = tl.parse_program(tokens)
+         local errors = tl.type_check(ast)
+         assert.same({}, errors)
+      end)
+      it("with nested ipairs", function()
+         local tokens = tl.lex([[
+            local t = { {"a", "b"}, {"c"} }
+            for i, a in ipairs(t) do
+               for j, b in ipairs(a) do
+                  print(i, j, "value: " .. b)
+               end
+            end
+         ]])
+         local _, ast = tl.parse_program(tokens)
+         local errors = tl.type_check(ast)
+         assert.same({}, errors)
+      end)
+      it("unknown with nested ipairs", function()
+         local tokens = tl.lex([[
+            local t = {}
+            for i, a in ipairs(t) do
+               for j, b in ipairs(a) do
+                  print(i, j, "value: " .. b)
+               end
+            end
+         ]])
+         local _, ast = tl.parse_program(tokens)
+         local errors = tl.type_check(ast)
+         assert.same({}, errors)
+      end)
    end)
    it("with an explicit iterator", function()
       local tokens = tl.lex([[
